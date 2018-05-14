817020e5a9de14477c97d611b00cd9d5416f3753
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-05-14 19:40:22 +0200

    login form v1

--- index.php
+++ index.php
@@ -5,8 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>The Complete Web Developer project</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.42/css/uikit.min.css" />
+
 </head>
 <body>
-    <h1>AWDGB</h1>
+
+    <div class="uk-section uk-container uk-text-center">
+        <div class="uk-flex uk-flex-center@s uk-flex-left uk-flex-stretch">
+            <form action="#" class="js-login" method="post">
+                <fieldset class="uk-fieldset">
+                    <legend class="uk-legend">Login</legend>
+                    <div class="uk-margin">
+                        <input type="text" class="uk-input" placeholder="Email" name="input_email">
+                    </div>
+                    <div class="uk-margin">
+                        <input type="password" class="uk-input" placeholder="Password" name="input_password">
+
+                        <button class="uk-button uk-button-default">Login</button>
+                    </div>
+
+                </fieldset>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.42/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.42/js/uikit-icons.min.js"></script>
 </body>
 </html>
